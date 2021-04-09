13cfa644f35f2e6eac84059824a97dd4f166dc11
João Fernandes <joaofnds@joaofnds.com>
2021-04-09 07:48:25 -0300

    revert: "revert: "feat(log): Add logger""
    This reverts commit 691a81810bda4856992944677212f18f64b80bbc.

--- foo/foo.go
+++ foo/foo.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/joaofnds/bar/logger"
 )
 
 var (
@@ -16,18 +18,30 @@ func init() {
 	if FOO_SERVICE_URL == "" {
 		panic("I need FOO_SERVICE_URL to perform :cheems:")
 	}
+
+	logger.InfoLogger().Println("foo service initialized")
 }
 
 // CallFoo calls the foo service
 func CallFoo() (string, error) {
 	client := http.Client{Timeout: 5 * time.Second}
+
+	start := time.Now()
+
 	resp, err := client.Get(FOO_SERVICE_URL)
+
+	elapsed := time.Since(start)
+	logger.InfoLogger().Printf("finished foo service call in %s", elapsed)
+
 	if err != nil {
+		logger.ErrorLogger().Printf("failed to read body: %+v\n", err)
 		return "", fmt.Errorf("failed to communicate with foo service: %w", err)
 	}
 	defer resp.Body.Close()
+
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
+		logger.ErrorLogger().Printf("failed to read body: %+v\n", err)
 		return "", fmt.Errorf("failed to read body: %w", err)
 	}
 
