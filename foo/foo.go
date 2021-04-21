3b94b935853edb2acd473e9cab7fe3c55ae9f53c
João Fernandes <joaofnds@joaofnds.com>
2021-04-20 23:38:04 -0300

    fix(debug): add a bunch of debug logs

--- foo/foo.go
+++ foo/foo.go
@@ -27,6 +27,7 @@ func init() {
 
 // CallFoo calls the foo service
 func CallFoo(ctx context.Context) (string, error) {
+	logger.DebugLogger().Println("CallFoo called")
 	client := http.Client{Timeout: 5 * time.Second}
 	req, err := http.NewRequest("GET", FOO_SERVICE_URL, nil)
 	if err != nil {
@@ -37,7 +38,9 @@ func CallFoo(ctx context.Context) (string, error) {
 	parentSpan := opentracing.SpanFromContext(ctx)
 	span := opentracing.StartSpan("GET "+FOO_SERVICE_URL, opentracing.ChildOf(parentSpan.Context()))
 	tracing.InjectRequestSpan(span, req)
+	logger.DebugLogger().Println("will perform foo service call")
 	resp, err := client.Do(req)
+	logger.DebugLogger().Println("got foo service response")
 	span.Finish()
 
 	if err != nil {
@@ -46,11 +49,14 @@ func CallFoo(ctx context.Context) (string, error) {
 	}
 	defer resp.Body.Close()
 
+	logger.DebugLogger().Println("will read bofy")
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		logger.ErrorLogger().Printf("failed to read body: %+v\n", err)
 		return "", fmt.Errorf("failed to read body: %w", err)
 	}
+
+	logger.DebugLogger().Println("foo call went ok")
 
 	return string(body), nil
 }
--- main.go
+++ main.go
@@ -24,11 +24,15 @@ func main() {
 	logger.InfoLogger().Println("Starting the application...")
 
 	host, _ := os.Hostname()
+	logger.DebugLogger().Println("got hostname")
 	closer := tracing.InitTracer(host)
+	logger.DebugLogger().Println("tracer initialized")
 	defer closer.Close()
 
 	http.HandleFunc("/", rootHandler)
+	logger.DebugLogger().Println("rootHandler registered")
 	http.HandleFunc("/health", healthHandler)
+	logger.DebugLogger().Println("healthHandler registered")
 
 	s := http.Server{Addr: ":3000"}
 	go func() {
@@ -45,20 +49,27 @@ func main() {
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
+	logger.DebugLogger().Println("rootHandler triggered")
 	span := tracing.StartSpanFromReq("rootHandler", opentracing.GlobalTracer(), r)
 	defer span.Finish()
+
+	logger.DebugLogger().Println("started span")
 
 	ctx := opentracing.ContextWithSpan(context.Background(), span)
 
 	host, _ := os.Hostname()
 
+	logger.DebugLogger().Println("will call foo")
 	response, err := foo.CallFoo(ctx)
+	logger.DebugLogger().Println("foo call finished")
 	if err != nil {
 		logger.ErrorLogger().Printf("failed to call foo service: %+v\n", err)
 		fmt.Fprintln(w, "Hello from "+host+", I failed to contact foo service")
 
 		return
 	}
+
+	logger.DebugLogger().Println("will respond")
 
 	fmt.Fprintln(w, "Hello from "+host+", here's what foo service said: "+response)
 }
