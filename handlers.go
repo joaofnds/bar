9dad62bbc450d16392aeee1a9a159cc97263736e
João Fernandes <joaofnds@joaofnds.com>
2021-05-03 18:19:13 -0300

    test: fix test

--- handlers.go
+++ handlers.go
@@ -24,12 +24,12 @@ func newFoohandler(name string, fooCaller fooCaller) http.HandlerFunc {
 		if err != nil {
 			logger.ErrorLogger().Printf("failed to call foo service: %+v\n", err)
 			w.WriteHeader(http.StatusFailedDependency)
-			fmt.Fprint(w, "Hello from ["+name+"], I failed to contact foo service")
+			fmt.Fprint(w, "Hello from "+name+", I failed to contact foo service")
 
 			return
 		}
 
-		fmt.Fprint(w, "Hello from "+name+", here's what foo service said: "+response)
+		fmt.Fprint(w, "Hello from ["+name+"], here's what foo service said: "+response)
 	}
 }
 
